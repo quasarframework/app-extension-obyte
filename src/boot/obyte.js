445bf229e806c8e3f92d98c74245b4597c32a661
gregory.latinier <gregory.latinier@gmail.com>
2019-08-08 19:13:45 +0200

    Prioritize the main net

--- src/boot/obyte.js
+++ src/boot/obyte.js
@@ -2,10 +2,10 @@ import obyte from 'obyte'
 
 export default ({ Vue }) => {
   let client
-if (process.env.OBYTE_TESTNET_API) {
-    client = new obyte.Client(process.env.OBYTE_TESTNET_API, { testnet: true })
-  } else if (process.env.OBYTE_API) {
+  if (process.env.OBYTE_API) {
     client = new obyte.Client(process.env.OBYTE_API)
+  } else if (process.env.OBYTE_TESTNET_API) {
+    client = new obyte.Client(process.env.OBYTE_TESTNET_API, { testnet: true })
   } else {
     client = new obyte.Client()
   }
